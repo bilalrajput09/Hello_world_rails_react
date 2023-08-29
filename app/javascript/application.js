55eecc2c2ac539672b75e64fe844ab76fd40952d
Bilal <ranabilalrajput09@gmail.com>
2023-08-29 14:21:54 +0200

    Add react configuration

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
-// Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './src/components/App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
